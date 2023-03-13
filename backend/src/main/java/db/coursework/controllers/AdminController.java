bd5f5d86c25716a90e472344c1875a6781e5daf4
exemee <dimamakhnin45640@gmail.com>
2023-03-13 19:58:00 +0100

    iteration 1 end

--- backend/src/main/java/db/coursework/controllers/AdminController.java
+++ backend/src/main/java/db/coursework/controllers/AdminController.java
@@ -1,24 +1,17 @@
 package db.coursework.controllers;
 
+import db.coursework.dto.*;
 import db.coursework.entities.*;
 import db.coursework.services.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -45,10 +38,10 @@ public class AdminController {
         this.addMaterialToOvumContainerService = addMaterialToOvumContainerService;
     }
 
-    @PostMapping("/get_predeterminers")
-    public ResponseEntity<List<HumanDTO>> getAllFutureJobTypesNames() {
+    @GetMapping("/users")
+    public ResponseEntity<List<HumanDTO>> getUsersByRoleName(@RequestParam String roleName) {
         try {
-            List<Human> predeterminers = humanService.findAllHumanByRoleName("ROLE_PREDETERMINER");
+            List<Human> predeterminers = humanService.findAllHumanByRoleName(roleName);
             List<HumanDTO> predeterminersDTO = predeterminers.stream().map(h -> new HumanDTO(h.getId(), h.getFullname())).collect(Collectors.toList());
             log.debug("Sending {} predeterminers", predeterminersDTO.size());
             return new ResponseEntity<>(predeterminersDTO, HttpStatus.OK);
@@ -57,10 +50,9 @@ public class AdminController {
         }
     }
 
-    @PostMapping("/get_orders")
-    public ResponseEntity<List<OrderDTO>> getAllHumanOrdersByFullname(@RequestBody Map<String, Long> payload) {
+    @GetMapping("/orders/human/{id}")
+    public ResponseEntity<List<OrderDTO>> getAllHumanOrdersById(@PathVariable Long id) {
         try {
-            Long id = payload.get("id");
             Human human = humanService.findHumanById(id);
             List<Order> orders = orderService.findAllOrdersByHuman(human);
             List<OrderDTO> orderDTOS = orders.stream().map(order -> new OrderDTO(order.getId(), order.getHumanNumber(), order.getCaste().name(), order.getFutureJobTypes().stream().map(type -> type.getName().toString()).collect(Collectors.toList()), order.isProcessing())).collect(Collectors.toList());
@@ -71,10 +63,13 @@ public class AdminController {
         }
     }
 
-    @PostMapping("/get_free_ovum_count")
-    public ResponseEntity<Long> getFreeOvumCount() {
+    @GetMapping("/ovums/count/order/{orderId}")
+    public ResponseEntity<Long> getOvumCountByOrder(@PathVariable String orderId) {
         try {
-            Long count = ovumService.getOvumCountByOrder(null);
+            Long id;
+            if (orderId.equals("null")) id = null;
+            else id = Long.valueOf(orderId);
+            Long count = ovumService.getOvumCountByOrderId(id);
             log.debug("Sending {} free ovum", count);
             return new ResponseEntity<>(count, HttpStatus.OK);
         } catch (Exception e) {
@@ -82,15 +77,14 @@ public class AdminController {
         }
     }
 
-    @PostMapping("/remove_extra_ovum_by_order")
-    public ResponseEntity<String> removeExtraOvumByOrder(@RequestBody Map<String, Long> payload) {
+    @DeleteMapping("/ovums/order/{orderId}")
+    public ResponseEntity<String> removeExtraOvumByOrder(@PathVariable Long orderId) {
         try {
-            Long orderId = payload.get("orderId");
             Order order = orderService.findOrderById(orderId);
             if (order == null)
                 throw new RuntimeException("Заказа не существует");
             //Уничтожаем "лишние"
-            Long ovumCount = ovumService.getOvumCountByOrder(order);
+            Long ovumCount = ovumService.getOvumCountByOrderId(order.getId());
             if (ovumCount > order.getHumanNumber()) {
                 log.debug("Количество яйцеклеток: {}. Требуется людей: {}", ovumCount, order.getHumanNumber());
                 ovumService.removeExtraOvumByOrderId(orderId, ovumCount - order.getHumanNumber());
@@ -102,11 +96,9 @@ public class AdminController {
         }
     }
 
-    @PostMapping("/bind_free_ovum")
-    public ResponseEntity<String> bindFreeOvum(@RequestBody Map<String, Long> payload) {
+    @PostMapping("/ovums/bind/{count}/order/{orderId}")
+    public ResponseEntity<String> bindFreeOvum(@PathVariable Long orderId, @PathVariable Long count) {
         try {
-            Long orderId = payload.get("orderId");
-            Long count = payload.get("count");
             log.debug("Bind {} ovum to order №{}", count, orderId);
             ovumService.bindFreeOvum(orderId, count);
             return new ResponseEntity<>("Началось выполнение заказа №" + orderId, HttpStatus.OK);
@@ -116,10 +108,10 @@ public class AdminController {
         }
     }
 
-    @PostMapping("/update_ovum")
+    @PutMapping("/ovums/{id}")
     public ResponseEntity<String> updateOvum(@RequestBody OvumDTO ovumDTO) {
         try {
-            log.debug("Обновляем яйцеклетку с id {}", ovumDTO.id);
+            log.debug("Обновляем яйцеклетку с id {}", ovumDTO.getId());
             ovumService.updateOvumByOvumDTOFields(ovumDTO.getId(), ovumDTO.isBud(), ovumDTO.getFertilizationTime(), ovumDTO.getEmbryoTime(), ovumDTO.getBabyTime());
             return new ResponseEntity<>("Яйцеклетка успешно обновлена", HttpStatus.OK);
 
@@ -129,10 +121,9 @@ public class AdminController {
         }
     }
 
-    @PostMapping("/get_use_machine")
-    public ResponseEntity<List<UseMachineByOvumContainerDTO>> getUseMachine(@RequestBody Map<String, Long> payload) {
+    @GetMapping("/use-machine-by-ovum-container/order/{orderId}")
+    public ResponseEntity<List<UseMachineByOvumContainerDTO>> getUseMachine(@PathVariable Long orderId) {
         try {
-            Long orderId = payload.get("orderId");
             log.debug("Получаем журнал использования машин контейнерами заказа {}", orderId);
             Order order = orderService.findOrderById(orderId);
             if (order == null) return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
@@ -152,10 +143,9 @@ public class AdminController {
         }
     }
 
-    @PostMapping("/get_move_container")
-    public ResponseEntity<List<MoveOvumContainerToRoomDTO>> getMoveContainer(@RequestBody Map<String, Long> payload) {
+    @GetMapping("/move-ovum-container-to-room/order/{orderId}")
+    public ResponseEntity<List<MoveOvumContainerToRoomDTO>> getMoveContainer(@PathVariable Long orderId) {
         try {
-            Long orderId = payload.get("orderId");
             log.debug("Получаем журнал передвижения контейнеров заказа {}", orderId);
             Order order = orderService.findOrderById(orderId);
             if (order == null) return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
@@ -175,10 +165,9 @@ public class AdminController {
         }
     }
 
-    @PostMapping("/get_add_material")
-    public ResponseEntity<List<AddMaterialToOvumContainerDTO>> getAddMaterial(@RequestBody Map<String, Long> payload) {
+    @GetMapping("/add-material-to-ovum-container/order/{orderId}")
+    public ResponseEntity<List<AddMaterialToOvumContainerDTO>> getAddMaterial(@PathVariable Long orderId) {
         try {
-            Long orderId = payload.get("orderId");
             log.debug("Получаем журнал добавления материалов в контейнеры заказа {}", orderId);
             Order order = orderService.findOrderById(orderId);
             if (order == null) return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
@@ -198,86 +187,14 @@ public class AdminController {
         }
     }
 
-    @PostMapping("/remove_order_info")
-    public ResponseEntity<String> removeOrder(@RequestBody Map<String, Long> payload) {
+    @DeleteMapping("/orders/{orderId}")
+    public ResponseEntity<String> removeOrder(@PathVariable Long orderId) {
         try {
-            Long orderId = payload.get("orderId");
             orderService.removeOrderInfo(orderId);
             return new ResponseEntity<>("Сведения о заказе удалены", HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>("Не удалось удалить заказ))))", HttpStatus.BAD_REQUEST);
         }
-    }
-
-
-    @Data
-    @AllArgsConstructor
-    private static class UseMachineByOvumContainerDTO {
-        @NotNull
-        Machine machine;
-        @NotNull
-        OvumContainer ovumContainer;
-        @NotNull
-        Date startTime;
-        @NotNull
-        Date endTime;
-        Integer totalBudsCount;
-    }
-
-    @Data
-    @AllArgsConstructor
-    private static class MoveOvumContainerToRoomDTO {
-        @NotNull
-        OvumContainer ovumContainer;
-        @NotNull
-        Room room;
-        @NotNull
-        Date arrivalTime;
-    }
-
-    @Data
-    @AllArgsConstructor
-    private static class AddMaterialToOvumContainerDTO {
-        @NotNull
-        Material material;
-        @NotNull
-        OvumContainer ovumContainer;
-        @NotNull
-        Date insertionTime;
-    }
-
-    @Data
-    @AllArgsConstructor
-    private static class HumanDTO {
-        Long id;
-        String fullname;
-    }
-
-    @Data
-    @AllArgsConstructor
-    private static class OvumDTO {
-        @NotNull
-        Long id;
-        @NotNull
-        private boolean isBud;
-        private Date fertilizationTime;
-        private Date embryoTime;
-        private Date babyTime;
-    }
-
-    @Data
-    @AllArgsConstructor
-    private static class OrderDTO {
-        @Null
-        private Long id;
-        @NotNull
-        private Integer humanNumber;
-        @NotNull
-        private String caste;
-        @NotNull
-        private List<String> futureJobTypes;
-        @NotNull
-        private boolean isProcessing;
     }
 }
